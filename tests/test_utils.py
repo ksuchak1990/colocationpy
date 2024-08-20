b04d61011ffc8a5606d4c10f61e4d4de37f4723b
Keiran Suchak <ksuchak1990@yahoo.co.uk>
2024-08-20 13:07:21 +0100

    Correct incomplete polygons in test data

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,3 +1,7 @@
+"""
+Test functions in `colocation.utils`
+"""
+
 from shapely.geometry import Polygon, LineString
 from colocation.utils import is_divided_by_barrier
 import pytest
@@ -6,9 +10,15 @@ import pytest
 barrier_divide_data = [
     ((0, 0), (1, 1), LineString([(1, 0), (0, 1)]), True),
     ((0, 0), (1, 1), LineString([(1, 0), (2, 1)]), False),
-    ((0, 0), (10, 10), Polygon([(0, 9), (1, 10), (10, 1), (9, 0)]), True),
-    ((0, 10), (10, 0), Polygon([(0, 9), (1, 10), (10, 1), (9, 0)]), True),
-    ((0, 0), (3, 3), Polygon([(0, 9), (1, 10), (10, 1), (9, 0)]), False),
+    ((0, 0), (10, 10), Polygon([(0, 9), (1, 10), (10, 1), (9, 0), (0, 9)]), True),
+    ((0, 10), (10, 0), Polygon([(0, 9), (1, 10), (10, 1), (9, 0), (0, 9)]), True),
+    ((0, 0), (3, 3), Polygon([(0, 9), (1, 10), (10, 1), (9, 0), (0, 9)]), False),
+    (
+        (0, 0),
+        (10, 10),
+        Polygon([(0, 4), (0, 5), (5, 5), (5, 0), (4, 0), (4, 4), (0, 4)]),
+        True,
+    ),
 ]
 
 
